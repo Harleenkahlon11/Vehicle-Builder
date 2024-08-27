9c9069097bb5bbaf5c826ccb3e31366fe0539844
Harleen Kahlon <harleenkahlon08@gmail.com>
2024-08-27 10:22:05 -0400

    declared properties with types,created constructor, called parent class,log details of Truck, used override modifier

--- src/classes/Motorbike.ts
+++ src/classes/Motorbike.ts
@@ -1,13 +1,13 @@
 // Importing Vehicle and Wheel classes
 import Vehicle from './Vehicle.js';
 import Wheel from './Wheel.js';
-// TODO: The Motorbike class should extend the Vehicle class
+//  The Motorbike class should extend the Vehicle class
 class Motorbike extends Vehicle {
   
-    // TODO: Declare properties of the Motorbike class
-    // TODO: The properties should include vin, color, make, model, year, weight, top speed, and wheels
+    // Declare properties of the Motorbike class
+    //  The properties should include vin, color, make, model, year, weight, top speed, and wheels
     // Declare properties with types
-    // TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[])
+    //  The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[])
       wheels: Wheel[];
       topSpeed: number;
       color: string;
@@ -17,12 +17,12 @@ class Motorbike extends Vehicle {
       weight: number;
       year: number;
 
-   // TODO: Create a constructor that accepts the properties of the Motorbike class
+   //  Create a constructor that accepts the properties of the Motorbike class
       constructor(vin: string, color: string, make: string, model: string, year: number, weight: number, topSpeed: number, wheels = []) {
      
-    // TODO: The constructor should call the constructor of the parent class, Vehicle
+    // The constructor should call the constructor of the parent class, Vehicle
       super();
-    // TODO: The constructor should initialize the properties of the Motorbike class
+    // The constructor should initialize the properties of the Motorbike class
         this.vin = vin;
         this.color = color;
         this.make = make;
@@ -31,22 +31,22 @@ class Motorbike extends Vehicle {
         this.weight = weight;
         this.topSpeed = topSpeed;
         this.wheels = wheels;
-        // TODO: The constructor should check if the wheels array has 2 elements and create 2 new default Wheel objects if it does not
-        // TODO: Implement the wheelie method
+        //  The constructor should check if the wheels array has 2 elements and create 2 new default Wheel objects if it does not
+        //Implement the wheelie method
         if (this.wheels.length !== 2) {
             this.wheels = [new Wheel(), new Wheel()];
         }
     }
-    // TODO: The method should log the message "Motorbike [make] [model] is doing a wheelie!"
+    //  The method should log the message "Motorbike [make] [model] is doing a wheelie!"
     wheelie() {
         console.log(`Motorbike ${this.make} ${this.model} is doing a wheelie!`);
     }
-    // TODO: Override the printDetails method from the Vehicle class
+    //  Override the printDetails method from the Vehicle class
     override printDetails() {
-        // TODO: The method should call the printDetails method of the parent class
+        //The method should call the printDetails method of the parent class
         super.printDetails();
-        // TODO: The method should log the details of the Motorbike
-        // TODO: The details should include the VIN, make, model, year, weight, top speed, color, and wheels
+        //  The method should log the details of the Motorbike
+        //  The details should include the VIN, make, model, year, weight, top speed, color, and wheels
         console.log(`VIN: ${this.vin}`);
         console.log(`Make: ${this.make}`);
         console.log(`Model: ${this.model}`);
